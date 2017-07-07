60335cc11d47950b8db796eeb4f39fa2a3a17957
TionChen <wangzuotion@gmail.com>
2017-07-07 22:26:22 +0800

    two functions did work when in code samples given by Udacity I wonder why

--- js/app.js
+++ js/app.js
@@ -268,15 +268,7 @@ function populateInfoWindow(marker, infowindow) {
     // In case the status is OK, which means the pano was found, compute the
     // position of the streetview image, then calculate the heading, then get a
     // panorama from that and set the options
-
-          // Use streetview service to get the closest streetview image within
-          // 50 meters of the markers position
-          streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
-          // Open the infowindow on the correct marker.
-          infowindow.open(map, marker);
-    }
-}
-function getStreetView(data, status) {
+    function getStreetView(data, status) {
         if (status == google.maps.StreetViewStatus.OK) {
               var nearStreetViewLocation = data.location.latLng;
               var heading = google.maps.geometry.spherical.computeHeading(
@@ -295,4 +287,12 @@ function getStreetView(data, status) {
               infowindow.setContent('<div>' + marker.name + '</div>' +
                 '<div>No Street View Found</div>');
             }
+    }
+          // Use streetview service to get the closest streetview image within
+          // 50 meters of the markers position
+          streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+          // Open the infowindow on the correct marker.
+          infowindow.open(map, marker);
+    }
 }
+    
